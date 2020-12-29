7bdd9dff12aeed23a00b9abaa5c1352d3ee798e1
github-action-benchmark <github@users.noreply.github.com>
2020-12-29 08:22:17 +0000

    add pipeline (cargo) benchmark result for 510b998ff3dc75e0dfc7e236212f5fef33142ad1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1609230095569,
+  "lastUpdate": 1609230136622,
   "repoUrl": "https://github.com/tmccombs/hyper",
   "entries": {
     "connect": [
@@ -29,6 +29,38 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 46849,
             "range": "± 3368",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "pipeline": [
+      {
+        "commit": {
+          "author": {
+            "email": "mussitantesmortem@gmail.com",
+            "name": "Maxim Zhukov",
+            "username": "Mephistophiles"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "510b998ff3dc75e0dfc7e236212f5fef33142ad1",
+          "message": "chore(dev-dependencies): bump url to 2.2 (#2379)",
+          "timestamp": "2020-12-28T15:52:28-08:00",
+          "tree_id": "cd9b46cf00546ee0223f287f33d1698b9c2ee161",
+          "url": "https://github.com/tmccombs/hyper/commit/510b998ff3dc75e0dfc7e236212f5fef33142ad1"
+        },
+        "date": 1609230128596,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 71475,
+            "range": "± 19970",
             "unit": "ns/iter"
           }
         ]
